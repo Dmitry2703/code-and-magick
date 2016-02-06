42cbbd4d804fe62d932ad55313c6d3dc80a329c3
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2016-02-06 16:22:15 +0600

    Module3-task1. Rev.1

--- js/form.js
+++ js/form.js
@@ -25,14 +25,64 @@
   };
 
   // Начальные условия
+  // Кнопка отправки формы заблокирована, так как поле Имя не заполнено
   formSubmitButton.setAttribute('disabled', 'disabled');
-
+  // Ссылка на поле Описание не отображается, если оценка выше 2
   for (var i = 0; i < formMark.length; i++) {
     if (formMark[i].checked) {
       if (formMark[i].value > 2) {
         formFieldsText.style.display = 'none';
       } else {
-        formFieldsText.style.display = 'inline';
+        formFieldsText.style.display = '';
+      }
+    }
+  }
+
+  // Проверка заполнения обязательных полей
+  function checkFormFields() {
+    // Поле Имя не заполнено
+    if (!formName.value) {
+      formSubmitButton.setAttribute('disabled', 'disabled');
+      formFieldsName.style.display = '';
+      formFields.style.display = '';
+      // Ссылка на поле Описание отображается/скрывается при незаполненном поле Имя
+      for (i = 0; i < formMark.length; i++) {
+        if (formMark[i].checked) {
+          if (formMark[i].value > 2) {
+            formFieldsText.style.display = 'none';
+          } else {
+            formFieldsText.style.display = '';
+            // Если оценка ниже 3, но поле Описание заполнено, то ссылка на поле Описание
+            // отображается/скрывается при незаполненном поле Имя
+            if (formText.value) {
+              formFieldsText.style.display = 'none';
+            }
+          }
+        }
+      }
+    // Поле Имя заполнено
+    } else {
+      formFieldsName.style.display = 'none';
+      // Поле Описание не заполнено
+      if (!formText.value) {
+        for (i = 0; i < formMark.length; i++) {
+          if (formMark[i].checked) {
+            if (formMark[i].value > 2) {
+              formSubmitButton.removeAttribute('disabled');
+              formFieldsText.style.display = 'none';
+              formFields.style.display = 'none';
+            } else {
+              formSubmitButton.setAttribute('disabled', 'disabled');
+              formFieldsText.style.display = '';
+              formFields.style.display = '';
+            }
+          }
+        }
+      // Поле Описание заполнено
+      } else {
+        formSubmitButton.removeAttribute('disabled');
+        formFieldsText.style.display = 'none';
+        formFields.style.display = 'none';
       }
     }
   }
@@ -42,7 +92,7 @@
     // Проверка заполнения поля Имя
     if (!formName.value) {
       formSubmitButton.setAttribute('disabled', 'disabled');
-      formFieldsName.style.display = 'inline';
+      formFieldsName.style.display = '';
     } else {
       formFieldsName.style.display = 'none';
       // Проверка заполнения поля Описание
@@ -56,44 +106,29 @@
               form.submit();
             } else {
               formSubmitButton.setAttribute('disabled', 'disabled');
+              formFieldsText.style.display = '';
+              formFields.style.display = '';
             }
           }
         }
       } else {
+        formSubmitButton.removeAttribute('disabled');
+        formFieldsText.style.display = 'none';
+        formFields.style.display = 'none';
         form.submit();
       }
     }
   };
 
   formName.oninput = function() {
-    if (formName.value) {
-      formSubmitButton.removeAttribute('disabled');
-      formFieldsName.style.display = 'none';
-    } else {
-      formFieldsName.style.display = 'inline';
-    }
+    checkFormFields();
   };
 
   formText.oninput = function() {
-    if (formText.value) {
-      formSubmitButton.removeAttribute('disabled');
-      formFieldsText.style.display = 'none';
-    } else {
-      formFieldsText.style.display = 'inline';
-    }
+    checkFormFields();
   };
 
   formMarkGroup.onchange = function() {
-    for (i = 0; i < formMark.length; i++) {
-      if (formMark[i].checked) {
-        if (formMark[i].value > 2) {
-          formSubmitButton.removeAttribute('disabled');
-          formFieldsText.style.display = 'none';
-        } else {
-          formSubmitButton.setAttribute('disabled', 'disabled');
-          formFieldsText.style.display = 'inline';
-        }
-      }
-    }
+    checkFormFields();
   };
 })();
